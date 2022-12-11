0987887c69f7e5e8be73666b8783e2f05207b51e
ptpuyen1511 <ptpuyen1511@gmail.com>
2022-12-12 01:46:35 +0700

    add spaceship config

---
+++ linux_tips/.spaceshiprc.zsh
@@ -0,0 +1,11 @@
+# Display time
+#SPACESHIP_TIME_SHOW=true
+
+# Display username always
+SPACESHIP_USER_SHOW=always
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
+
+SPACESHIP_USER_COLOR="#ff87ff"
+# https://upload.wikimedia.org/wikipedia/commons/1/15/Xterm_256color_chart.svg
